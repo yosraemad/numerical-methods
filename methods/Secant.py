df4e9162fc5e27c3fa4aa7b11b8a2170075f2dfb
Muhhamed Sallam <m.sallam2622000@gmail.com>
2021-11-21 20:54:10 +0200

    secant first code

--- methods/Secant.py
+++ methods/Secant.py
@@ -1,3 +1,28 @@
+from tkinter.constants import TRUE
+def f(x):
+    return x**4 - 2*x**3 - 4*x**2 - 4*x +4
+
 def secant(fun_details):
-    print("Secant")
-    #TODO
+    precision = float(0.01)
+    maxIterations = 10
+    print("Secant\n")
+
+    iteration = 0;
+    xl = float(-1);
+    xu = float(0);
+    xold = 0
+
+    while TRUE:
+        if f(xu) == f(xl):
+            print('Error! division by zero')
+            break
+        iteration += 1 
+        x = float(xl - f(xl)*(xu-xl) / (f(xu) - f(xl)))
+        error = float(abs(x-xold)/abs(x))
+        print('iteration %d  a= %0.10f  b= %0.10f  f(a)= %0.10f  f(b)= %0.10f  x= %0.10f  error= %0.10f\n' % (iteration,xl,xu,f(xl),f(xu),x,error))
+        if(iteration>=maxIterations or error<precision):
+            print('Approximate root is %0.10f' %x)
+            break
+        xl = xu
+        xu = x
+        xold = x 
